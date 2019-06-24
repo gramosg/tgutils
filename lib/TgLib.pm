57f8e009519b305294d002784967239e7a41ef5e
Guillermo Ramos <0xwille@gmail.com>
2019-06-24 13:10:02 +0200

    TgLib::fetch_token

--- lib/TgLib.pm
+++ lib/TgLib.pm
@@ -1,6 +1,20 @@
 package TgLib;
 
+use TgLib::Env qw<$CONFIG_HOME>;
+
 use parent qw<Exporter>;
-our @EXPORT = qw<>;
+our @EXPORT = qw<fetch_token>;
+
+sub fetch_token {
+    my $token = $ENV{'TGUTILS_TOKEN'};
+    unless ($token) {
+        open(my $cfg, "<", "$CONFIG_HOME/tgutils_token") or return;
+        $token = <$cfg>;
+        chomp $token;
+        close $cfg;
+    }
+    $token =~ /^[0-9]+:[a-zA-Z0-9]+$/ or die "Invalid bot token ($token)";
+    return $token;
+}
 
 1;
--- lib/TgLib/Env.pm
+++ lib/TgLib/Env.pm
@@ -1,10 +1,11 @@
 package TgLib::Env;
 
 use parent qw<Exporter>;
-our @EXPORT = qw<$HOME $CONFIG_HOME $CACHE_HOME>;
+our @EXPORT = qw<$HOME $CONFIG_HOME $CACHE_HOME $TOKEN>;
 
 our $HOME = $ENV{'HOME'};
 our $CONFIG_HOME = $ENV{'XDG_CONFIG_HOME'} || "$HOME/.config";
 our $CACHE_HOME = $ENV{'XDG_CACHE_HOME'} || "$HOME/.cache";
+our $TOKEN = $ENV{'TGUTILS_TOKEN'};
 
 1;
