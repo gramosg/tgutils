d27d603d01b025d1bb85e463601428736d01f273
Guillermo Ramos <0xwille@gmail.com>
2019-06-25 02:07:12 +0200

    No empty responses, Logger::warn

--- lib/TgLib/Logger.pm
+++ lib/TgLib/Logger.pm
@@ -1,9 +1,9 @@
 package TgLib::Logger;
 
 use parent qw<Exporter>;
-our @EXPORT = qw<new debug info>;
+our @EXPORT = qw<new debug info warn>;
 
-my %level = (info => 1, debug => 2);
+my %level = (warn => 0, info => 1, debug => 2);
 
 sub log_level {
     my ($self, $msg, $level) = @_;
@@ -20,5 +20,6 @@ sub new {
 
 sub debug { log_level(@_, 'debug'); }
 sub info { log_level(@_, 'info'); }
+sub warn { log_level(@_, 'warn'); }
 
 1;
