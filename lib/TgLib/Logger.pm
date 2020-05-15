0deb9dbd3897e81fb0b7287af9b8322e0d5e5263
Guillermo Ramos <gramos@gramos.me>
2020-05-15 19:51:14 +0200

    Why the space in [level ]? No reason.

--- lib/TgLib/Logger.pm
+++ lib/TgLib/Logger.pm
@@ -12,7 +12,7 @@ my %level = (warn => 0, info => 1, debug => 2);
 sub log_level {
     my ($self, $msg, $level) = @_;
     my $date = strftime "%F %X", localtime;
-    print STDERR "[$level ] $date: $msg" if $level{$level} <= $self->{'level'};
+    print STDERR "[$level] $date: $msg" if $level{$level} <= $self->{'level'};
 }
 
 ################################################################################
